d15f7d4f87c4aa0245b39f2d10736d0830a395b6
Jana Simashkevich <jsimashk@gmail.com>
2023-11-14 16:00:06 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ import requests
 streamlit.header("Fruityvice Fruit Advice!")
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-#streamlit.text(fruityvice_response.json())
+streamlit.text(fruityvice_response.json())
 
 
 # write your own comment -what does the next line do? 
